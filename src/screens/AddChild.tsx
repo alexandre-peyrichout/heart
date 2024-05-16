4a1338123187ed51dfa7ebd4d1197fc5af22c18c
Alexandre Peyrichout <a.peyrichout@gmail.com>
2024-05-16 14:51:09 +0200

    add child clean

--- src/screens/AddChild.tsx
+++ src/screens/AddChild.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Alert, Button, Image, TextInput, View } from "react-native";
+import {
+  Alert,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import DateTimePicker, {
   DateTimePickerEvent,
@@ -8,6 +15,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import * as ImagePicker from "expo-image-picker";
 import { addDoc, collection } from "firebase/firestore";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import Animated, { FadeInDown } from "react-native-reanimated";
 import uuid from "react-native-uuid";
 
 import { useAuth } from "../context/Auth";
@@ -18,8 +26,8 @@ type Props = NativeStackScreenProps<RootStackParamList, "AddChild">;
 
 export default function AddChild({ navigation }: Props) {
   const [name, setName] = useState("");
-  const [birthDate, setBirthDate] = useState(new Date(1598051730000));
-  const [image, setImage] = useState("");
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [image, setImage] = useState(null);
   const { loggedInUser } = useAuth();
 
   const pickImage = async () => {
@@ -75,28 +83,56 @@ export default function AddChild({ navigation }: Props) {
     };
 
     createChildDocument();
-    navigation.reset({
-      index: 0,
-      routes: [{ name: "Home" }],
-    });
+    navigation.navigate("Home");
   };
 
   return (
-    <View className="bg-white w-full h-full flex justify-around">
-      <TextInput
-        placeholder="Name"
-        value={name}
-        onChangeText={handleNameChange}
-      />
+    <View className="bg-white w-full h-full flex justify-between">
+      <View className="m-4 space-y-4">
+        <Animated.View
+          entering={FadeInDown.delay(200).duration(1000).springify()}
+          className="bg-black/5 p-5 rounded-2xl w-full"
+        >
+          <TextInput
+            placeholder="Name"
+            placeholderTextColor={"gray"}
+            value={name}
+            onChangeText={handleNameChange}
+          />
+        </Animated.View>
+        <Animated.View
+          entering={FadeInDown.delay(200).duration(1000).springify()}
+          className=" p-5 rounded-2xl w-full flex-row justify-between items-center"
+        >
+          <Text>Birth Date: </Text>
+          <DateTimePicker
+            mode="date"
+            value={birthDate}
+            onChange={handleBirthDateChange}
+          />
+        </Animated.View>
 
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} className="w-32 h-32 mx-auto" />}
-      <DateTimePicker
-        mode="date"
-        value={birthDate}
-        onChange={handleBirthDateChange}
-      />
-      <Button title="Submit" onPress={handleSubmit} />
+        {image ? (
+          <Image source={{ uri: image }} className="w-40 h-40 mx-auto" />
+        ) : (
+          <TouchableOpacity
+            className="w-40 h-40 mx-auto bg-gray-400 justify-center"
+            onPress={pickImage}
+          >
+            <Text className="text-white text-center">Upload image</Text>
+          </TouchableOpacity>
+        )}
+
+        <TouchableOpacity
+          disabled={!name || !birthDate || !image}
+          onPress={handleSubmit}
+          className="bg-black w-full p-3 mb-3 rounded-2xl"
+        >
+          <Text className="text-white font-bold text-xl text-center">
+            Add child
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
